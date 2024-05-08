2d7686f9a9dd7c13ed5b85b2b16879aef538e863
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2024-05-08 18:27:38 +0530

    Update fitness_bot.py

--- fitness_bot.py
+++ fitness_bot.py
@@ -1,37 +1,39 @@
 import streamlit as st
 import google.generativeai as genai
-# from langchain import PromptTemplate, FewShotPromptTemplate
 from dotenv import load_dotenv
-load_dotenv()
 import os
 
+# Load environment variables
+load_dotenv()
 
-
-
+# Get API key from environment variable
 API_KEY = os.getenv("GOOGLE_GEMINI_API")
 
+# Configure the API
 genai.configure(api_key=API_KEY)
 
+# Create the GenerativeModel instance
 model = genai.GenerativeModel(model_name="gemini-pro")
 
-# st.header("Personal Fitness Bot 🤖")
-
+# Streamlit UI
 st.markdown("<h1 style='text-align: center;'>Personal Fitness Bot 🤖</h1>", unsafe_allow_html=True)
-# st.markdown("<h5 style='text-align: center;'>🚀 A fitness personal chatbot</h5>", unsafe_allow_html=True)
-
 st.caption("🚀 A fitness personal chatbot")
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
+# User input
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "human", "content": prompt})
     st.chat_message("user").write(prompt)
-    print("prompt",st.session_state.messages)
-    # response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = model.generate_content(prompt).text
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
 
+    # Generate content
+    try:
+        msg = model.generate_content(prompt).text
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
+    except Exception as e:
+        st.error(f"Error generating content: {str(e)}")
